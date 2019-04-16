6207c87f3ebc10aa16221ade20bb5159a0d18b9d
Jonathan Daly <dalyj138@gmail.com>
2019-04-16 18:57:48 -0400

    Project init && finished actions/reducers. Added react-loader-spinner.

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { FETCHING, SUCCESS, FAILURE } from '../actions';
+import { getPeople } from '../actions';
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -14,8 +16,9 @@ class CharacterListView extends React.Component {
   }
 
   render() {
-    if (this.props.fetching) {
+    if (this.props.isFetching) {
       // return something here to indicate that you are fetching data
+      <Loader type="Ball-Triangle" color="#00BFFF" height="100" width="65" />
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +30,16 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => ({
+  characters: state.characters,
+  isFetching: state.isFetching
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,/* mapStateToProps replaces null here */
   {
     /* action creators go here */
+    getPeople
   }
 )(CharacterListView);
